730d916bb96c1a13bf9f8ad558e73940cb02625e
pands <vikbon7@gmail.com>
2017-03-26 19:30:56 +0300

    обновление

--- src/com/pands/Main.java
+++ src/com/pands/Main.java
@@ -21,7 +21,7 @@ public class Main {
 
         d=Math.sqrt(b*b+4*a*c) ;
 
-            ans= ((b+d)/2a -(a ^ 3 * c + b ^ -2))
+            ans= ((b+d)/2*a) -(Math.pow(a,3) * c + Math.pow (b,-2));
 
 
             System.out.println("ans="+ans);
